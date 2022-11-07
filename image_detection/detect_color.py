c883237f666e4022769494b62dab60953beaa741
njzhu <njzhu@andrew.cmu.edu>
2022-11-07 17:40:46 -0500

    better color detection

--- image_detection/detect_color.py
+++ image_detection/detect_color.py
@@ -1,4 +1,5 @@
 # code used from https://pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+# https://stackoverflow.com/questions/47483951/how-to-define-a-threshold-value-to-detect-only-green-colour-objects-in-an-image
 import numpy as np
 import argparse
 import cv2
@@ -9,16 +10,21 @@ args = vars(ap.parse_args())
 # load the image
 image = cv2.imread(args["image"])
 
-boundaries = [([0, 0, 0], [100, 255, 75])]
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-(lower, upper) = boundaries[0]
-lower = np.array(lower, dtype = "uint8")
-upper = np.array(upper, dtype = "uint8")
+# boundaries = [([36, 25, 25], [70, 255, 25])]
 
-mask = cv2.inRange(image, lower, upper)
-output = cv2.bitwise_and(image, image, mask = mask)
-print(mask)
-print(output)
+# (lower, upper) = boundaries[0]
+# lower = np.array(lower, dtype = "uint8")
+# upper = np.array(upper, dtype = "uint8")
 
-cv2.imshow("images", np.hstack([image, output]))
+mask = cv2.inRange(hsv, (36, 25, 25), (70, 255, 255))
+imask = mask > 0
+green = np.zeros_like(image, np.uint8)
+green[imask] = image[imask]
+
+cv2.imshow("images", np.hstack([image, green]))
+
+# gray = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
+# thresh = 
 cv2.waitKey(0)
